09db95c61e7ee9cda6e4827320b73b4452b8adbd
jangdm37 <jangdm37@gmail.com>
2023-05-07 11:44:35 +0900

    .

--- study_weekend/react/hooks/src/hooks/timer-modal.js
+++ study_weekend/react/hooks/src/hooks/timer-modal.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 
 const TimerModal = () => {
+    let memo = 0;
+    const [count,setCount] = useState(0)
+    useEffect(()=>{
+        memo =5;
+        console.log("timer mount!")
+    },[])
     return (
         <div>
-            0
+            {count}
         </div>
     );
 };
--- study_weekend/react/hooks/src/hooks/useEffect.js
+++ study_weekend/react/hooks/src/hooks/useEffect.js
@@ -4,13 +4,16 @@ import TimerModal from './timer-modal';
 const UseEffect = () => {
     const [isOpenTimerModal,setIsOpenTimerModal] =useState(true)
     useEffect(()=>{
+        if (!isOpenTimerModal) return;
         console.log("mount")
+        console.log("real",isOpenTimerModal)
+
     },[isOpenTimerModal]);
     return (
         <div>
             {isOpenTimerModal && <TimerModal/>}
             <button onClick={()=>{
-                console.log(isOpenTimerModal)
+                // console.log(isOpenTimerModal)
                 setIsOpenTimerModal((prev)=>!prev)}}>재실행</button>
         </div>
     );
@@ -21,5 +24,6 @@ export default UseEffect;
 /* 
 1) 페이지가 mount 되었을때 실행 = window.onload
 2) state는 비동기로 동작 -> state 변화 이후 실행할 effect 관리
-3)
+3) 페이지가 unmount 될 때는 return을 사용하여 컴포넌트가 돔에서 제거되었을때 실행할 effect를 관리
+
 */
